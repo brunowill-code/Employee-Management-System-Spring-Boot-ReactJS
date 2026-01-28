cc0d4217e5059182eab886838f2ec595b68db425
brunowill-code <brunowilliam.code@gmail.com>
2026-01-28 14:30:14 -0300

    feat: Endpoints

--- src/main/java/app/ems_backend/resources/EmployeeResource.java
+++ src/main/java/app/ems_backend/resources/EmployeeResource.java
@@ -1,9 +1,21 @@
 package app.ems_backend.resources;
 
+import java.time.temporal.Temporal;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import app.ems_backend.entities.Employee;
 import app.ems_backend.services.EmployeeService;
 
 @RestController
@@ -12,5 +24,29 @@ public class EmployeeResource {
 	
 	@Autowired
 	private EmployeeService employeeService;
+	
+	@GetMapping
+	public ResponseEntity<List<Employee>> getEmployee(){
+		List<Employee> employees = employeeService.getAllEmloyee();
+		return ResponseEntity.ok().body(employees);
+	}
+	
+	@PostMapping
+	public ResponseEntity<List<Employee>> postEmployee(@RequestBody Employee obj){
+		List<Employee> employees = employeeService.addEmployee(obj);
+		return ResponseEntity.ok().body(employees);
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee obj){
+		Employee employees = employeeService.updateEmployee(id, obj);
+		return ResponseEntity.ok().body(employees);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deleteEmployee(@PathVariable Long id){
+		employeeService.deleteEmplooye(id);
+		return ResponseEntity.noContent().build();
+	}
 	
 }
--- src/main/java/app/ems_backend/services/EmployeeService.java
+++ src/main/java/app/ems_backend/services/EmployeeService.java
@@ -1,8 +1,11 @@
 package app.ems_backend.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import app.ems_backend.entities.Employee;
 import app.ems_backend.repositories.EmployeeRepository;
 
 @Service
@@ -11,6 +14,46 @@ public class EmployeeService {
 	@Autowired
 	private EmployeeRepository employeeRepository;
 	
+	public List<Employee> getAllEmloyee(){
+		return employeeRepository.findAll();
+	}
 	
+	public List<Employee> addEmployee(Employee obj){
+		employeeRepository.save(obj);
+		return getAllEmloyee();
+	}
+	
+	public Employee updateEmployee(Long id, Employee obj){
+		if(!employeeRepository.existsById(id)) {
+			throw new RuntimeException();
+		}
+		
+		try {
+			Employee update = employeeRepository.getReferenceById(id);
+			updateEmployee(update, obj);
+			return employeeRepository.save(update);
+		} catch(RuntimeException e){
+			throw e;
+		}
+	}
+	
+	public void deleteEmplooye(Long id) {
+		if(!employeeRepository.existsById(id)) {
+			throw new RuntimeException();
+		}
+		
+		employeeRepository.deleteById(id);
+		
+	}
+	
+	
+
+	private void updateEmployee(Employee update, Employee obj) {
+
+		update.setEmail(obj.getEmail());
+		update.setFirstName(obj.getFirstName());
+		update.setLastName(obj.getLastName());
+		
+	}
 
 }
