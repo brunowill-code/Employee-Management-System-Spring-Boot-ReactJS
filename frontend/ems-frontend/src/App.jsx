b51f11eb8b715db425753749b8b4d031e7345e9b
brunowill-code <brunowilliam.code@gmail.com>
2026-01-28 20:46:01 -0300

    feat: Create React EmployeeComponent & Add Route

--- frontend/ems-frontend/src/App.jsx
+++ frontend/ems-frontend/src/App.jsx
@@ -1,12 +1,24 @@
 
 import './App.css'
+import EmployeeComponent from './assets/components/EmployeeComponent'
+import FooterComponent from './assets/components/FooterComponent'
+import HeaderComponent from './assets/components/HeaderComponent'
 import ListEmployeeComponent from './assets/components/ListEmployeeComponent'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <ListEmployeeComponent />
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes >
+          <Route path='/' element = {<ListEmployeeComponent />} ></Route>
+          <Route path='/employees' element = {<ListEmployeeComponent />}></Route>
+          <Route path='/add-employee' element= {<EmployeeComponent />}></Route>
+        </Routes>
+        <FooterComponent />
+      </BrowserRouter>
     </>
   )
 }
